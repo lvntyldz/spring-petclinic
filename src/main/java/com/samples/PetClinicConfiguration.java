68bd07e57a48d22730ffcc8ac67006fd8e242d3c
levent yıldız <leventyildiz@leventyildiz.local>
2018-05-31 20:03:09 +0300

    Access Properties File With Java Class

---
+++ src/main/java/com/samples/PetClinicConfiguration.java
@@ -0,0 +1,19 @@
+package com.samples;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PetClinicConfiguration {
+
+	@Autowired
+	private PetClinicProperties petClinicProperties;
+	
+	@PostConstruct
+	public void init() {
+	System.out.println("Display Owners With Peths : "+petClinicProperties.isDisplayOwnersWithPets());
+	}
+	
+}
